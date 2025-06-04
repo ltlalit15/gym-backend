1648cb9ca93edc6e7e9168682af1681c093ec07d
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-04 17:08:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,39 +1,19 @@
-// local mysql
+
+// live server
 
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-    host: '127.0.0.1',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'gym',
-    multipleStatements: true
+    host: 'gondola.proxy.rlwy.net',
+    port: 32521,                    
+    user: 'root',                   
+    password: 'yNhhVAiBMtCVSMqAqIsKDgWDNqhGvRsr',  
+    database: 'gym',                
+    multipleStatements: true       
 });
 
- 
-
-console.warn('Connected');
+console.warn('Connected to Railway MySQL Database');
 
 module.exports = db;
-
-
-// live server
-
-// const mysql = require('mysql2/promise');
-
-// // Update the connection details with the new URL and credentials
-// const db = mysql.createPool({
-//     host: 'turntable.proxy.rlwy.net',  // Updated host URL
-//     port: 21573,                        // Updated port
-//     user: 'root',                       // MySQL username
-//     password: 'dYVNOmeRrbfJkCVkXnMwGuMKRPZsTVtP',  // Updated password
-//     database: 'railway',                // Updated database name
-//     multipleStatements: true           // Allow multiple statements if needed
-// });
-
-// console.warn('Connected');
-
-// module.exports = db;
 
 
