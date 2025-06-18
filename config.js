15a48e63f55f8cd1b60411025ecd91861560a23f
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-18 19:18:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,12 +4,12 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-    host: 'gondola.proxy.rlwy.net',
-    port: 32521,                    
-    user: 'root',                   
-    password: 'yNhhVAiBMtCVSMqAqIsKDgWDNqhGvRsr',  
-    database: 'railway',                
-    multipleStatements: true       
+    host: 'shuttle.proxy.rlwy.net',   // Updated host
+    port: 32074,                      // Updated port
+    user: 'root',                     // User
+    password: 'zkfLLtwLtzgONVIHWnOHNpnfMAGeuIeL', // Updated password
+    database: 'railway',              // Database name
+    multipleStatements: true         // Allow multiple SQL statements
 });
 
 console.warn('Connected to Railway MySQL Database');
